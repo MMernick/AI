e37bd6d8dcac8c494002d332182750b3770fcc80
MMernick <mernick@live.com>
2015-10-02 11:00:10 -0300

    Atualização de nome de variavel.

--- php/carregar_cidades.php
+++ php/carregar_cidades.php
@@ -1,10 +1,10 @@
 <?php
 include '../config_path.php';
 
-$str_data = file_get_contents($GLOBALS['PATH'].'/registros/cidades.json');
-$data = json_decode($str_data,true);
+$cidades_data = file_get_contents($GLOBALS['PATH'].'/registros/cidades.json');
+$cidades = json_decode($cidades_data,true);
 
-for($i = 0; $i < count($data); $i++){
-    echo "<span class=\"glyphicon glyphicon-flag icone\"></span>&nbsp&nbsp&nbsp".$data[$i]."<br>";
+for($i = 0; $i < count($cidades); $i++){
+    echo "<span class=\"glyphicon glyphicon-flag icone\"></span>&nbsp&nbsp&nbsp".$cidades[$i]."<br>";
 }
 
