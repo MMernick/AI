57526ef151af600dcaf43df996fbbacdb4581c56
MMernick <mernick@live.com>
2015-10-03 19:00:34 -0300

    Atualização (Inclusão de Validação - Somente Numeros no Campo KM)

--- js/core.js
+++ js/core.js
@@ -70,3 +70,12 @@ function upload_arquivo(id_input, php_alvo){
   document.body.appendChild(frame);
   setTimeout(envia_arquivo, 250);
 };
+
+function SomenteNumero(e){
+    var tecla=(window.event)?event.keyCode:e.which;   
+    if((tecla>47 && tecla<58)) return true;
+    else{
+    	if (tecla==8 || tecla==0) return true;
+	else  return false;
+    }
+}
